f67eec50d6e01f6768a47e45a24dbe146b1642fa
Vatsal Jain <vj92@drexel.edu>
2022-04-30 22:31:44 -0400

    homepage without how to use and about us

--- dragonhacks/urls.py
+++ dragonhacks/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,4 +7,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage, name="homepage"),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
